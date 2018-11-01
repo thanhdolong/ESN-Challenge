bea2af749820e7d0a174055f733be1fca7c3092d
thanhdolong <thanh.dolong@gmail.com>
2018-11-01 21:29:07 +0100

    Separate ulities from controler

--- challenge/Map screens/Controllers/MapSreenViewController.swift
+++ challenge/Map screens/Controllers/MapSreenViewController.swift
@@ -122,13 +122,3 @@ extension MapScreenViewController: CLLocationManagerDelegate {
         print("region error \(error.localizedDescription)")
     }
 }
-
-// MARK: Helper Extensions
-extension UIViewController {
-    func showAlert(withTitle title: String?, message: String?) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-        alert.addAction(action)
-        present(alert, animated: true, completion: nil)
-    }
-}
