b3d06b2f53fc530ed1f325c447dcce083cfa9378
thanhdolong <thanh.dolong@gmail.com>
2018-11-06 14:22:01 +0100

    Setup color with hex or RGB values

--- challenge/App/Controllers/Utilities.swift
+++ challenge/App/Controllers/Utilities.swift
@@ -19,10 +19,22 @@ extension UIViewController {
     }
 }
 
+// MARK: Zoom user location Extensions
 extension MKMapView {
     func zoomToUserLocation() {
         guard let coordinate = userLocation.location?.coordinate else { return }
         let region = MKCoordinateRegion.init(center: coordinate, latitudinalMeters: 10000, longitudinalMeters: 10000)
         setRegion(region, animated: true)
+    }
+}
+
+// MARK: Setup color with hex or RGB values
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
     }
 }
