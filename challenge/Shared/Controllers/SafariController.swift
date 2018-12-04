c5b75d614b0221c63eb77a7e62f84ea78cb7cbb8
thanhdolong <thanh.dolong@gmail.com>
2018-12-04 23:11:18 +0100

    Add safari controller

---
+++ challenge/Shared/Controllers/SafariController.swift
@@ -0,0 +1,31 @@
+//
+//  Safari.swift
+//  challenge
+//
+//  Created by Thành Đỗ Long on 26/11/2018.
+//  Copyright © 2018 Thành Đỗ Long. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import SafariServices
+
+class SafariController: NSObject {
+    func openUrl(url: URL) {
+        let safariVC = SFSafariViewController(url: url)
+        safariVC.delegate = self
+//        safariVC.preferredControlTintColor = Appearance.shared().mainColor^
+        
+        if let topController = UIApplication.shared.keyWindow?.rootViewController {
+//            UIApplication.shared.statusBarView?.backgroundColor = NavigationController.defaultStatusBarColor
+            topController.present(safariVC, animated: true, completion: nil)
+        }
+    }
+}
+
+extension SafariController: SFSafariViewControllerDelegate {
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+//        UIApplication.shared.statusBarView?.backgroundColor = .white
+    }
+}
