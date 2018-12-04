fd3d12011f77559404c624d4e8232755bd138760
thanhdolong <thanh.dolong@gmail.com>
2018-12-04 23:06:37 +0100

    Add high scores screen

---
+++ challenge/High scores screen/Controllers/HighScoresViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Thành Đỗ Long on 29/10/2018.
+//  Copyright © 2018 Thành Đỗ Long. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import SwinjectStoryboard
+
+class HighScoresViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    static func storyboardInit() -> UIViewController {
+        let storyboard = UIStoryboard(name: "HighScores", bundle: nil)
+        let viewController = storyboard.instantiateInitialViewController()! 
+        return viewController
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+
+    
+    @IBAction func showMap(_ sender: UIButton) {
+        print("show map")
+        let viewController = MapViewController.storyboardInit()
+        navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+}
+
