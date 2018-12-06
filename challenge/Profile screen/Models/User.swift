a7db1251e7f8297368d3696f886344bb186bf494
thanhdolong <thanh.dolong@gmail.com>
2018-12-06 21:24:17 +0100

    Add view controller

---
+++ challenge/Profile screen/Models/User.swift
@@ -0,0 +1,33 @@
+//
+//  User.swift
+//  challenge
+//
+//  Created by Thành Đỗ Long on 05/12/2018.
+//  Copyright © 2018 Thành Đỗ Long. All rights reserved.
+//
+
+import Foundation
+
+final class User {
+    var accessToken: String? {
+        get {
+            return UserDefaults.standard.string(forKey: "accessToken")
+        }
+        
+        set {
+            UserDefaults.standard.set(newValue, forKey: "accessToken")
+        }
+    }
+    
+    func isAccessTokenExist() -> Bool {
+        guard let acessToken = accessToken  else {
+            return false
+        }
+        
+        return acessToken.isEmpty ?  false : true
+    }
+    
+    func deleteAccessToken() {
+        UserDefaults.standard.removeObject(forKey: "accessToken")
+    }
+}
