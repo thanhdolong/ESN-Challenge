d5cff988b0a4b1ea8df161d2e483e0ca6e5d2e24
thanhdolong <thanh.dolong@gmail.com>
2019-02-14 16:59:59 +0100

    login screen

--- challenge/Login screen/Controllers/LoginViewController.swift
+++ challenge/Login screen/Controllers/LoginViewController.swift
@@ -8,39 +8,56 @@
 
 import UIKit
 
+protocol LoginViewControllerDelegate: class {
+    func userLogged()
+}
+
 class LoginViewController: UIViewController {
     private let safariController = SafariController()
-    private let locationAPI: LocationAPI = LocationAPI()
+    private let api: ChallengeAPI = ChallengeAPI()
+    private let token: Token = Token()
+//    var profileViewController: ProfileViewController!
+    var delegate: LoginViewControllerDelegate?
     
     var loginView: LoginView! {
         guard isViewLoaded else { return nil }
         return (view as! LoginView)
     }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
     @IBAction func forgotPassword(_ sender: UIButton) {
-        guard let url = URL(string: "https://fiesta.esncz.org/password/forgot/") else {
+        guard let url = URL(string: "https://challenge.esncz.org/password/forgot/") else {
             return
         }
         safariController.openUrl(url: url)
     }
     
     @IBAction func signup(_ sender: UIButton) {
-        guard let url = URL(string: "https://fiesta.esncz.org/setup") else {
+        guard let url = URL(string: "https://challenge.esncz.org/setup") else {
             return
         }
         safariController.openUrl(url: url)
     }
     
-    static func storyboardInit() -> UIViewController {
+    static func storyboardInit() -> LoginViewController {
         let storyboard = UIStoryboard(name: "Login", bundle: nil)
-        let viewController = storyboard.instantiateInitialViewController()!
+        let viewController = storyboard.instantiateInitialViewController() as! LoginViewController
         return viewController
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupNavigation()
+    }
+    
+    private func setupNavigation() {
+        let settingsImage = UIImage(named: "settings")?.withRenderingMode(.alwaysTemplate)
+        let settingsButton = UIBarButtonItem(image: settingsImage, style: .plain, target: self, action: #selector(openSettings))
+        navigationItem.rightBarButtonItem = settingsButton
+    }
+    
+    @objc func openSettings() {
+        let vc = NavigationController(rootViewController: SettingsTableViewController.init())
+        self.present(vc, animated: true, completion: nil)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -49,23 +66,24 @@ class LoginViewController: UIViewController {
 
     @IBAction func login() {
         guard let email = loginView.emailTextField.text, !email.isEmpty else {
-            return showAlert(withTitle: nil, message: "The email is incorrect.")
+            return showAlert(withTitle: nil, message: "The email can not be empty.")
         }
         
         guard let password = loginView.passwordTextField.text, !password.isEmpty else {
-            return showAlert(withTitle: nil, message: "The password is incorrect.")
+            return showAlert(withTitle: nil, message: "The password can not be empty")
         }
         
         loginView.isLoginButtonEnabled(false)
         
-        locationAPI.login(email: email, password: password) { (data, error) in
+        api.login(email: email, password: password) { (data, error) in
             self.loginView.isLoginButtonEnabled(true)
             
             guard let data = data as? [String: String] else {
                 return self.showAlert(withTitle: nil, message: error?.errorMessages ?? "Unknown error. Please, try it again later.")
             }
-            
-            Default.accessToken = data["access_token"]
+
+            self.token.accessToken = data["access_token"]
+            self.delegate?.userLogged()
         }
     }
 }
